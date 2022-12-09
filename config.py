8c026a443190b4d4372d4c61e919fe304ad990ea
JoaquimBreno <winz993@gmail.com>
2022-12-08 23:38:31 -0300

    URL db

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from db import DATABASE_URL
 
-#DATABASE_URI = "postgresql://boadlfsiavrxeb:e743d7b2ab5b25f237e8326cecf8e27b487686821f168ea7fac6ebed4bd4deb4@ec2-44-207-133-100.compute-1.amazonaws.com:5432/da9obqdvpm8te8"
-DATABASE_URI = DATABASE_URL
+DATABASE_URI = "postgresql://boadlfsiavrxeb:e743d7b2ab5b25f237e8326cecf8e27b487686821f168ea7fac6ebed4bd4deb4@ec2-44-207-133-100.compute-1.amazonaws.com:5432/da9obqdvpm8te8"
+#DATABASE_URI = DATABASE_URL
 engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
